c4331f5a8b852a4cb0b9b5df88ee353eb26359af
JhondhelPago <jhondhelpago2307@gmail.com>
2024-10-04 01:55:37 +0800

    testing the rest_framework classes

--- userauth/views.py
+++ userauth/views.py
@@ -2,12 +2,23 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+import json
+
 
 # Create your views here.
 
 @api_view(['POST'])
 def login(request):
-    return Response({'message' : 'login view responding'})
+
+    data = json.loads(request.body)
+
+    username = data.get('username')
+    password = data.get('password')
+
+    return Response({
+        'your_username' : username,
+        'your_password' : password
+    })
 
 
 
