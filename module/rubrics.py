a5b08e448e22b30b22b47024a5fc26823d2a6ee6
JhondhelPago <jhondhelpago2307@gmail.com>
2024-10-10 10:49:54 +0800

    rubrics_module

---
+++ module/rubrics.py
@@ -0,0 +1,53 @@
+#import libraries
+
+
+
+
+#helping functions
+
+
+
+class Ideas:
+
+    def mainfunction():
+
+        pass
+
+
+
+class Gram_Punc:
+
+    def mainfunction():
+
+        pass
+
+
+class Transition:
+
+    def mainfunction():
+
+        pass
+
+class Clarity:
+
+    def mainfunction():
+
+        pass
+
+class WordChoice:
+
+    def mainfunction():
+
+        pass
+
+class Structure:
+
+    def mainfunction():
+
+        pass
+
+class Lang_Mechs:
+
+    def mainfunction():
+
+        pass
