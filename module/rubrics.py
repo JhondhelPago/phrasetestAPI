588df7b6819398cb2c535aa58b5c4ddd073e89c3
JhondhelPago <jhondhelpago2307@gmail.com>
2024-10-11 12:34:27 +0800

    trying to figure out the Ideas criterion on the rubrics module

--- module/rubrics.py
+++ module/rubrics.py
@@ -1,4 +1,7 @@
 #import libraries
+from sentence_transformers import SentenceTransformer, util
+from rake_nltk import Rake
+
 
 
 
@@ -9,9 +12,29 @@
 
 class Ideas:
 
-    def mainfunction():
+    @staticmethod
+    def testingSimilarity(text_list):
 
-        pass
+        model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+
+        sentence_list = text_list
+
+        embiddings = model.encode(sentence_list, convert_to_tensor=True)
+        similarity = util.pytorch_cos_sim(embiddings[0], embiddings[1])
+
+        return similarity
+
+
+    #return a list of key words that relate to the topic or the contenxt of the essay composition
+    @staticmethod
+    def rake_KeyWord(text): 
+
+        r = Rake()
+        r.extract_keywords_from_text(text)
+
+        keywords = r.get_ranked_phrases()
+        return keywords
+
 
 
 
--- module/test_LanguageTool.py
+++ module/test_LanguageTool.py
@@ -1,5 +1,6 @@
 from LanguageToolChecker import LangToolChecker
 from features_xtrct import PhraseExtract
+from rubrics import Ideas
 
 # [
 #     'message',
@@ -29,14 +30,14 @@ from features_xtrct import PhraseExtract
 # return the list
 
 
-
 question = 'Write your thought about the technology advancement.'
-sampleEssay = 'The advancments in technolagy have revolutionized the way we comunicate and access information. With the rise of smartphons, tablets, and computers, people can now conect with others around the globe instanly. However, this rapid devlopment also comes with some challenges, such as the increase in cybercrime and the growing dependency on digital devices. As technolagy continues to evolve, it is crucial for societys to find a balance between embracing innovation and ensuring securty.'
+sampleEssay = 'Technology has profoundly transformed education by enhancing accessibility, quality, and flexibility in learning. With the advent of digital tools, education has become more inclusive, allowing students from diverse locations and backgrounds to access the same learning opportunities through online platforms. No longer confined to physical classrooms, learners can now participate in virtual classrooms, access e-books, and engage with educational content at their own pace. This flexibility benefits both students and teachers by offering personalized learning experiences and broadening access to resources. Moreover, technological advancements such as interactive whiteboards and educational apps have enriched teaching methods, making learning more engaging and effective. Consequently, technology continues to break barriers and bridge educational inequalities, creating a more connected, informed, and adaptable global learning environment.'
 
-
+"""
 response_result = LangToolChecker(sampleEssay)
 
 print(response_result)
+"""
 
 
 
@@ -45,6 +46,32 @@ print(response_result)
 
 
 # print(EssayObject.doc_sm)
+
+
+
+phraseObject = PhraseExtract(question=question, text=sampleEssay)
+
+sentence_list = phraseObject.ArrayOfSents()
+
+
+
+print(sentence_list)
+
+
+"""
+result = Ideas.testingSimilarity(sentence_list)
+
+print(result)
+
+"""
+
+print('\n\n\n')
+
+
+print('this is the keywords found in the sentence using the rake_nltk')
+keyword = Ideas.rake_KeyWord(phraseObject.text)
+
+print(keyword)
 
 
 
