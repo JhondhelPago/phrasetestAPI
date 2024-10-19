8183d7758e5ee04b9d58cae59c7895ffb3016250
JhondhelPago <jhondhelpago2307@gmail.com>
2024-10-19 16:27:45 +0800

    PhrasetestExtract.TopicRelevence is subject for more testing of function

--- module/test2_sentencevariation.py
+++ module/test2_sentencevariation.py
@@ -4,8 +4,8 @@ from LanguageToolChecker import LangToolChecker
 
 
 
-question = 'What is your biggest fear?'
-essay = 'Reading is a rewarding habit. It opens doors to new worlds, ideas, and knowledge, and it helps people relax. Some prefer reading in the morning, while others enjoy it in the evening, but either way, it provides a much-needed escape from stress. For instance, reading a novel can help someone unwind after a long day, which might improve sleep. Although some people find reading difficult, with practice, anyone can get better at it. In the end, those who read regularly are likely to grow both personally and professionally.'
+question = 'When you imagine yourself in 20 years, where do you want to be?'
+essay = 'In 20 years I imagined myself as a successful software engineer and a businessman. I dreamed to work as programmer at global tech companies like Google, Microsoft, and Apple. After 20 years, I also think that I already have agricultural businesses. During my age of 20s it is the time when i have to get married and start my own family, but before all of it I need to make sure that i have enough resources to support family\'s needs. I also want to explore my career opportunity overseas. I know that there a lot to discover on myself during my age of 20\'s and continue to improve my life quality.'
 
 
 phraseInstance = PhraseExtract(question=question, text=essay)
@@ -16,3 +16,7 @@ result = phraseInstance.SentenceVariationAnalyzer(with_sent_index=True)
 print('\n\nresuts')
 
 print(result)
+
+print('\ntopic relevance')
+
+print(phraseInstance.TopicRelevance())
