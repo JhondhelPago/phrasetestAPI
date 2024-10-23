1dbec0617d53d0f07e1b035f0fccd26e8f820cba
JhondhelPago <jhondhelpago2307@gmail.com>
2024-10-23 17:19:08 +0800

    Scripting the EssayExamineErrorSuggest

--- module/LanguageToolChecker.py
+++ module/LanguageToolChecker.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+from features_xtrct import PhraseExtract
+
 LanguageToolAPI = 'https://api.languagetool.org/v2/check'
 
 def LangToolChecker(text):
@@ -26,12 +28,75 @@ def LangToolChecker(text):
     
 
 
-#classes for structuring the response result of the LanguageToolAPI
+def EssayExamineErrorSuggest(PhraseInstance : PhraseExtract):
 
-# class ErrorSpotter:
+    
 
-#     def __init__(self, doc) -> None:
-#         self.doc = 
+    match_result =  LangToolChecker(PhraseInstance.text)
+
+    match_result = [
+        Match(
+            message=result['message'],
+            shortMessage=result['shortMessage'],
+            replacements=result['replacements'],
+            offset=result['offset'],
+            length=result['length'],
+            context=result['context'],
+            sentence=result['sentence'],
+            type=result['type'],
+            rule=result['rule'],
+            ignoreForIncompleteSentence=result['ignoreForIncompleteSentence'],
+            contextForSureMatch=result['contextForSureMatch']
+        ) 
+        for result in match_result
+    ]
+
+
+    sentence_list = PhraseInstance.ArrayOfSents()
+
+
+    print(match_result)
+    print(isinstance(match_result[0], Match))
+
+
+
+    print('\n\n')
+
+    print(sentence_list)
+
+    return True
+
+
+class Match:
+
+    def __init__(self, message, shortMessage, replacements, offset, length, context, sentence, type, rule, ignoreForIncompleteSentence, contextForSureMatch):
+    
+        self.message = message
+        self.shortMessage =  shortMessage
+        self.replacements = replacements
+        self.offset = offset
+        self.length = length
+        self.context = context
+        self.sentence = sentence
+        self.type = type
+        self.rule = rule
+        self.ignoreForIncompleteSentence = ignoreForIncompleteSentence
+        self.contextForSureMatch = contextForSureMatch
+
+
+        self.belongsToSentenceId = None
+
+    def setBelongsToSentenceId(self, id: int):
+
+        self.belongsToSentenceId = id
+
+
+
+
+
+
+
+    
 
 
 
---
+++ module/test/test41LangToolAPI.py
@@ -0,0 +1,21 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+from features_xtrct import PhraseExtract
+from LanguageToolChecker import LangToolChecker, EssayExamineErrorSuggest
+
+
+question = 'When you imagine yourself in 20 years, where do you want to be?'
+essay = 'In 20 years I imagined myself as a successful software engineer and a businessman. I dreamed to work as programmer at global tech companies like Google, Microsoft, and Apple. After 20 years, I also think that I already have agricultural businesses. During my age of 20s it is the time when i have to get married and start my own family, but before all of it I need to make sure that i have enough resources to support family\'s needs. I also want to explore my career opportunity overseas. I know that there a lot to discover on myself during my age of 20\'s and continue to improve my life quality.'
+
+
+Phrase = PhraseExtract(question=question, text=essay)
+
+
+
+return_value = EssayExamineErrorSuggest(PhraseInstance=Phrase)
+
+
