45a0874748a6658b4e4d441aecdd55ccb8a29b33
JhondhelPago <jhondhelpago2307@gmail.com>
2024-11-11 23:20:02 +0800

    updated requirements.txt

--- module/ml_model.py
+++ module/ml_model.py
@@ -4,13 +4,13 @@ import random
 
 def predict_writing_level(writing_composition: str):
 
-    labels = ['underdeveloped', 'mediocre', 'advanced']
+    labels = ['below average', 'average', 'above average']
 
     return random.choice(labels)
 
 def predict_level(features_dict : dict) -> str:
 
-    labels = ['underdeveloped', 'mediocre', 'advanced']
+    labels = ['below average', 'average', 'above average']
 
     return random.choice(labels)
 
