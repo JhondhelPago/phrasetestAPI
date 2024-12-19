1a7e4b47fb031b54f09de394be27ca0c7be66fd9
JhondhelPago <jhondhelpago2307@gmail.com>
2024-12-19 16:06:39 +0800

    Comment and algo for the Vocabulary Assessment

---
+++ module/app_feature.py
@@ -0,0 +1,14 @@
+# Vocabulary Assessment
+# Contextual Understanding
+# Difficulty Assesment
+
+
+
+
+
+# Vocab_Assess class what will accepth PhraseExtract object instance.
+# getting the count of the POS
+# find the pos the the composition is lacking
+# Indentify the the repeated word and make and suggest the alternate word to enrich the vocabulary
+# Output - List of redundant word with its alternative replacement.
+
